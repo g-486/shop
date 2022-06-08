4b5630a1836d3806dcc1867cd76eca30d6d0f002
grl <690238795@qq.com>
2022-06-08 23:46:08 +0800

    test

---
+++ app/src/main/java/com/app/shop/ui/activity/newActivity.java
@@ -0,0 +1,13 @@
+package com.app.shop.ui.activity;
+
+import com.app.shop.base.BaseActivity;
+
+/**
+ * create by 呵呵 on 2022/6/8.
+ */
+public class newActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+}
